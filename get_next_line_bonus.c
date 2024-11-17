caca05a06b6208c38f9c96bc6e6368e16ac97a05
dixneuf <zaryprepa@gmail.com>
2024-11-17 03:16:04 +0100

    Add files via upload

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mzary <mzary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:40:39 by mzary             #+#    #+#             */
-/*   Updated: 2024/11/09 21:38:48 by mzary            ###   ########.fr       */
+/*   Updated: 2024/11/17 02:58:09 by mzary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,6 @@ void	check_plus(t_buff *p_plus)
 	}
 }
 
-static void	set_zero(t_buff *p_plus)
-{
-	ssize_t	i;
-
-	i = 0;
-	while (i < BUFFER_SIZE)
-	{
-		(p_plus->content)[i] = 0;
-		i++;
-	}
-	p_plus->length = 0;
-	p_plus->end = 0;
-	p_plus->next = NULL;
-}
-
 static char	*get_line(int fd, t_buff *p_plus)
 {
 	t_buff	*p_buff;
@@ -61,7 +46,7 @@ static char	*get_line(int fd, t_buff *p_plus)
 		p_buff->length = read(fd, p_buff->content, BUFFER_SIZE);
 		if (p_buff->length == -1)
 		{
-			set_zero(p_plus);
+			p_plus->length = 0;
 			free(p_buff);
 			free_buff(p_plus);
 			return (NULL);
