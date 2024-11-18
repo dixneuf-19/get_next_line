584ffa217b518428f8fdae6d577ab7d581b89b05
dixneuf <zaryprepa@gmail.com>
2024-11-18 04:58:18 +0100

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -22,8 +22,7 @@ static void	check_plus(t_buff *p_plus, fd)
 	{
 		if ((p_plus->content)[i] == '\n')
 		{
-			if (read(fd, NULL, 0) == 0)
-				p_plus->end = 1;
+			p_plus->end = 1;
 			break ;
 		}
 		i++;
@@ -61,7 +60,7 @@ char	*get_next_line(int fd)
 {
 	static t_buff	plus[OPEN_MAX];
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= OPEN_MAX)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= OPEN_MAX || read(fd, NULL, 0) == -1)
 		return (NULL);
 	plus[fd].next = NULL;
 	return (get_line(fd, plus + fd));
